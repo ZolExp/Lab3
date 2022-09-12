a74aeda72bcb058e945986d5006932644a7989c7
ZolExp <dmitry.zolotov.spb@gmail.com>
2022-09-12 23:04:35 +0300

    Create "dopsa" folder. Fix some bugs in Sign in and up.

--- Site/script.js
+++ Site/script.js
@@ -1,6 +1,14 @@
 document.querySelector("button").onclick = myClick;
 
 function myClick() {
-    let email = document.querySelector("email").value
+    let email = document.querySelector(".email").value
     document.querySelector(".out").innerHTML = email
 }
+
+function transfer(){
+    var a = forma.text3.value;
+   location.href = "newhtml.html?"+a;
+}
+
+var a=location.search.substring(1);
+forma.text3.value=a;
